348dc9f47147ada7bf465b93474959c45f4380c1
yingjiege <127132937+yingjiege@users.noreply.github.com>
2023-07-03 09:09:34 -0400

    Update LoginPage.jsx

--- my-app/src/components/login/LoginPage.jsx
+++ my-app/src/components/login/LoginPage.jsx
@@ -1,14 +1,20 @@
 import React, { Fragment, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link,useNavigate } from "react-router-dom";
 import axios from "axios";
 import * as Realm from "realm-web";
+import NavbarBeforeLogin from "../navbar/NavbarBeforeLogin";
 
 import "../../App.css";
+
+const REALM_APP_ID = "application-0-hxfdv"; // e.g. myapp-abcde
+const app = new Realm.App({ id: REALM_APP_ID });
 
 export default function SignInPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const app = new Realm.App({ id: "application-0-hxfdv" });
+  const [user, setUser] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const navigate = useNavigate();
 
   const onChangeEmail = (e) => {
     setEmail(e.target.value);
@@ -18,47 +24,49 @@ export default function SignInPage() {
     setPassword(e.target.value);
   };
 
-  const onSubmit = (e) => {
+  async function emailLogin(e) {
     e.preventDefault();
-    const data = {
-      email: email,
-      password: password,
-    };
-    axios
-      .post(
-        "https://us-east-1.aws.data.mongodb-api.com/app/application-0-hxfdv/endpoint/login",
-        data
-      )
-      .then((response) => {
-        console.log(response.data);
-        window.location.href = '/'+response.data[0]['_id'];
-      })
-      .catch((e) => {
-        console.log(e);
-      });
-  };
-
-  const google = (response) => {
-// The redirect URL should be on the same domain as this app and
-      // specified in the auth provider configuration.
-      const redirectUrl = "https://codesandbox.io/s/blue-sun-ngrm77?file=/src/App.js";
-      const credentials = Realm.Credentials.google({ redirectUrl });
-      // Calling logIn() opens a Google authentication screen in a new window.
-      app
-        .logIn(credentials)
-        .then((user) => {
-          // The logIn() promise will not resolve until you call `handleAuthRedirect()`
-          // from the new window after the user has successfully authenticated.
-          console.log(`Logged in with id: ${user.id}`);
-        })
-        .catch((err) => console.error(err));
-  };
+    try {
+      const credentials = Realm.Credentials.emailPassword(email, password);
+      // Authenticate the user
+      const userinfo = await app.logIn(credentials);
+      setUser(userinfo);
+      // `App.currentUser` updates to match the logged in user
+          const data = {
+            email: email,
+            password: password,
+            user_id: userinfo.id,
+          };
+          await axios
+            .post(
+              "https://us-east-1.aws.data.mongodb-api.com/app/application-0-hxfdv/endpoint/login",
+              data
+            )
+            .then((response) => {
+              if (response.data[0].allow_login) {
+                localStorage.setItem('user', response.data[0].user_id);
+                navigate('/shop');
+              } else {
+                navigate('/home');
+              }
+            })
+            .catch((e) => {
+              console.log(e);
+              setErrorMessage(e.error.toString());
+            });
+    } catch (error) {
+      //console.error('Failed to register user:', error);
+      console.log(error.error.toString());
+      setErrorMessage(error.error.toString());
+    }
+  }
 
   return (
     <Fragment>
+      <NavbarBeforeLogin/>
       <div
         className="modal modal-sheet position-static d-block p-4 py-md-5"
-        tabindex="-1"
+        tabIndex="-1"
         role="dialog"
         id="modalSignin"
       >
@@ -66,12 +74,6 @@ export default function SignInPage() {
           <div className="modal-content rounded-4 shadow">
             <div className="modal-header p-5 pb-4 border-bottom-0">
               <h1 className="fw-bold mb-0 fs-2">Sign in to us</h1>
-              <button
-                type="button"
-                className="btn-close"
-                data-bs-dismiss="modal"
-                aria-label="Close"
-              ></button>
             </div>
 
             <div className="modal-body p-5 pt-0">
@@ -83,9 +85,9 @@ export default function SignInPage() {
                     id="floatingInput"
                     placeholder="name@example.com"
                     value={email}
-              onChange={onChangeEmail}
+                    onChange={onChangeEmail}
                   />
-                  <label for="floatingInput">Username or email address</label>
+                  <label>email address</label>
                 </div>
                 <div className="form-floating mb-3">
                   <input
@@ -94,38 +96,20 @@ export default function SignInPage() {
                     id="floatingPassword"
                     placeholder="Password"
                     value={password}
-              onChange={onChangePassword}
+                    onChange={onChangePassword}
                   />
-                  <label for="floatingPassword">Password</label>
+                  <label>Password</label>
                   <Link to="/forget-password">
-                    <label classNameName="right-label">Forget password?</label>
+                    <label className="right-label">Forget password?</label>
                   </Link>
                 </div>
+                <div style={{ color: "red" }}>{errorMessage}</div>
                 <button
                   className="w-100 mb-2 btn btn-lg rounded-3 btn-primary"
                   type="submit"
-                  onClick={onSubmit}
+                  onClick={emailLogin}
                 >
                   Sign in
-                </button>
-                <small className="text-body-secondary">
-                  By clicking Sign up, you agree to the terms of use.
-                </small>
-                <hr className="my-4" />
-                <h2 className="fs-5 fw-bold mb-3">Or use a third-party</h2>
-                <button
-                  className="w-100 py-2 mb-2 btn btn-outline-secondary rounded-3"
-                  type="submit"
-                  onClick={google}
-                >
-                  Sign in with Google
-                  <i classNameName="bi bi-google-fill btn"></i>
-                </button>
-                <button
-                  className="w-100 py-2 mb-2 btn btn-outline-primary rounded-3"
-                  type="submit"
-                >
-                  Sign in with Facebook
                 </button>
               </form>
               <footer>
