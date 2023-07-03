7f6b72510e6a6f04deb01e61490ebe0061bdf1dc
yingjiege <127132937+yingjiege@users.noreply.github.com>
2023-07-03 09:13:34 -0400

    Update App.jsx

--- my-app/src/components/App.jsx
+++ my-app/src/components/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import LandingPage from './LandingPage'
 import LoginPage from './login/LoginPage'
@@ -7,31 +7,35 @@ import ForgetPasswordPage from './login/ForgetPasswordPage'
 import HomePage from './HomePage'
 import Report from './report/Report'
 import AccountSetting from './profile/AccountSetting'
-import Navbar from './navbar/Navbar.jsx'
 import Toggle from './shop/Toggle'
 import CheckOut from './shop/checkout/CreateOrder'
 import OrderCompleted from './shop/orderComplete/orderCompleted'
+import Testing1 from './testing1'
+import PendingAccount from './login/PendingAccount'
+
 
 import '../App.css'
 import "bootstrap/dist/css/bootstrap.css";
 
 export default function App() {
+    const [info, setInfo] = useState({})
     return (
         <Router>
             <div>
-            <Navbar/>
                 <Routes>
                     <Route path="/:userid" element={<HomePage/>}/>
                     <Route path="/" element={ <LandingPage/> } />
-                    <Route path="/login" element={ <LoginPage/> } />
+                    <Route path="/login" element={ <LoginPage/> } setInfo = {setInfo}/>
                     <Route path="/register" element={ <RegisterPage/> } />
                     <Route path="/forget-password" element={ <ForgetPasswordPage/> } />
                     <Route path="/home" element={ <HomePage/> } />
                     <Route path="/report" element={ <Report/> } />
                     <Route path="/account-setting" element={ <AccountSetting/> } />
-                    <Route path="/shop" element={ <Toggle/> } />
+                    <Route path="/shop" element={ <Toggle/> } info = {info}/>
                     <Route path="/cart" element={<CheckOut/>}/>
                     <Route path="/ordercompleted" element={<OrderCompleted/>}/>
+                    <Route path="/test" element={ <Testing1/> } />
+                    <Route path="/accountmanagement" element={ <PendingAccount/> } />
                 </Routes>
             </div>
         </Router>
